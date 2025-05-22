ca2fab7b70ef131fde4bd956528f0885047f2a13
Ethan Corey <ethanscorey@gmail.com>
2025-05-22 00:13:58 -0400

    Simple test

--- __tests__/ping.test.js
+++ __tests__/ping.test.js
@@ -1,10 +1,10 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
 
-describe('GET /api/ping', () => {
-  it('responds with 200 and pong', async () => {
-    const res = await request(app).get('/api/ping');
+describe("GET /api/ping", () => {
+  it("responds with 200 and pong", async () => {
+    const res = await request(app).get("/api/ping");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('pong');
+    expect(res.text).toBe("pong");
   });
 });
